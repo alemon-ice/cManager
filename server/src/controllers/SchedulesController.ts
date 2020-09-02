b9631ed0fe2e14ed8b2ae7ad0647d6ce9fb54047
Rafael Dias <rafaeldiasdesouza.rd@gmail.com>
2020-09-02 00:26:59 -0300

    :sparkles: Update Schedule
    Create user update route

--- server/src/controllers/SchedulesController.ts
+++ server/src/controllers/SchedulesController.ts
@@ -100,4 +100,91 @@ export default class SchedulesController {
       return response.send({ error: err });
     }
   };
+
+  update = async (request: Request, response: Response): Promise<Response> => {
+    const { id } = request.params;
+    const {
+      title,
+      description,
+      date,
+      start_time,
+      end_time,
+      is_important,
+    } = request.body;
+
+    const trx = await connection.transaction();
+
+    const scheduleStartDateTime = parseISO(`${date}T${start_time}`);
+    const scheduleEndDateTime = parseISO(`${date}T${end_time}`);
+
+    if (isPast(scheduleStartDateTime)) {
+      await trx.rollback();
+      return response.send(
+        'A data definida já passou e não pode ser selecidonada.',
+      );
+    }
+
+    const schedulesOnDate = await trx('schedules')
+      .where('date', date)
+      .select('id', 'start_time', 'end_time');
+
+    const CheckIftimeIsValid = (
+      schedulesArray: {
+        id: string;
+        start_time: string;
+        end_time: string;
+      }[],
+    ) => {
+      const scheduleExists = schedulesArray.filter(schedule => {
+        const existingScheduleStartDateTime = parseISO(
+          `${date}T${schedule.start_time}`,
+        );
+        const existingScheduleEndDateTime = parseISO(
+          `${date}T${schedule.end_time}`,
+        );
+
+        if (
+          ((isAfter(scheduleStartDateTime, existingScheduleStartDateTime) &&
+            isBefore(scheduleStartDateTime, existingScheduleEndDateTime)) ||
+            (isAfter(scheduleEndDateTime, existingScheduleStartDateTime) &&
+              isBefore(scheduleEndDateTime, existingScheduleEndDateTime)) ||
+            (isBefore(scheduleStartDateTime, existingScheduleStartDateTime) &&
+              isAfter(scheduleEndDateTime, existingScheduleEndDateTime))) &&
+          schedule.id !== id
+        ) {
+          return true;
+        }
+      });
+
+      return scheduleExists;
+    };
+
+    if (schedulesOnDate.length > 0) {
+      const [timeIsValid] = CheckIftimeIsValid(schedulesOnDate);
+
+      if (timeIsValid) {
+        await trx.rollback();
+        return response.send('Já existe um agendamento para este horário.');
+      }
+    }
+
+    const schedule = {
+      title,
+      description,
+      date,
+      start_time,
+      end_time,
+      is_important,
+    };
+
+    try {
+      await trx('schedules').update(schedule).where({ id });
+
+      await trx.commit();
+
+      return response.status(200).send('Agendamento atualizado com sucesso');
+    } catch (err) {
+      return response.send({ error: err });
+    }
+  };
 }
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -8,5 +8,6 @@ const schedulesController = new SchedulesController();
 
 app.get('/schedules', schedulesController.index);
 app.post('/schedules', schedulesController.create);
+app.put('/schedules/:id', schedulesController.update);
 
 export default app;
